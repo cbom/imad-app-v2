5bfbaabc049f830c12c141651d78fc07b5262506
cbom <matrixzodiac9919@gmail.com>
2017-02-10 09:53:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
-app.get('/ui/madi.png', function (req, res) {
+app.get('/ui/anjali0.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'anjali0.png'));
 });
 
