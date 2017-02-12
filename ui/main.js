ba300ebd77f8855ff6466d1a3bcc95f0df7e9e19
cbom <matrixzodiac9919@gmail.com>
2017-02-12 17:28:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,2 +1,15 @@
 //Counter code
-var button =  
+var button = document.getElementById('counter');
+var counter = 0;
+
+button.onclick = function () {
+    
+    //Make a request to the counter endpoint
+    
+    //Capture the response and store it in a variable
+    
+    //render the variable in the correct span
+    counter = counter+1;
+    var span= document.getElementbyId('count');
+    span.innerHTML = counter.toString();
+};
