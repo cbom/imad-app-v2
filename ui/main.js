19d4c4b3ea96295b6f2fae4ff90a5f9cf409f078
cbom <matrixzodiac9919@gmail.com>
2017-02-12 20:05:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,26 +4,26 @@ var button = document.getElementById('counter');
 //button.onclick = function () {
     
     //Create the request object 
-    var request = new XMLHttpRequest();
+    //var request = new XMLHttpRequest();
     
     //Capture the response and store it in a variable
-    request.onreadystatechange = function()
-    {
-        if (request.readyState === XMLHttpRequest.DONE)
-        {
+    //request.onreadystatechange = function()
+    //{
+      //  if (request.readyState === XMLHttpRequest.DONE)
+        //{
             //Take the action
-            if(request.status === 200){
-                var counter = request.responseText;
-                var span = document.getElementById('count');
-            span.innerHTML = counter.toString();
-          }
-        }
+          //  if(request.status === 200){
+            //    var counter = request.responseText;
+              //  var span = document.getElementById('count');
+            //span.innerHTML = counter.toString();
+          //}
+        //}
         //Not done yet
-      };
+      //};
       //Make the request
-      request.open('GET', 'http://cbom.imad.hasura-app.io/counter');
-      request.send(null);
-    };
+     // request.open('GET', 'http://cbom.imad.hasura-app.io/counter');
+      //request.send(null);
+    //};
 
 
 //submit name
